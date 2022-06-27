9145a7df61952769e499c5aaeaf110f0fe8e485e
Juan Cruz Dieguez <juancruzdieguez95@gmail.com>
2022-06-27 10:45:46 -0300

    Desafio 1 JavaScript

---
+++ main.js
@@ -0,0 +1,25 @@
+leerNumero='';
+numero=0;
+total=0;
+
+leerNumero = prompt('Ingrese el primer numero');
+console.log('Los numeros sumados fueron');
+while(leerNumero !='0' && leerNumero!=null ){
+    if(leerNumero == '' || leerNumero<='z' && leerNumero>='a'){
+        alert('Ingrese un número, Por favor');
+        leerNumero = prompt('Ingrese el primer numero');
+    }else{
+        numero = parseInt(leerNumero);
+        console.log(numero)
+        total = total + numero;
+        leerNumero = prompt('Siga sumando o aprete = para el resultado');
+    }
+}
+if(leerNumero==undefined){
+    console.log('Se cancelo la suma... :(')
+    alert('Se canceló la suma... :(')
+}else{
+    console.log('El total es: ', total)
+    alert('La suma total a pagar es: ' + total)
+
+}
